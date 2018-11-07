eec7ab695a2df31ddab4c1a4981f5fdc8ab962c0
Jordan Rand <jorrand@gmail.com>
2018-11-07 11:30:10 -0700

    making progress...

--- django-project/blog/views.py
+++ django-project/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def blog(request):
+    queryset = Post.objests.all().order_by("-date")[:25]
     return render(request, "blog.html", {})
+
+def post(request):
+    return render(request, 'post.html', {})
